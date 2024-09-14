1d55cab3c021c68bcb033501f98527592b05df7b
Muhammad Umer <rawalaumer@gmail.com>
2024-09-14 11:16:40 +0500

    frontend bug

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
     }
   };
   return (
-    <div className="flex flex-col min-h-screen bg-black">
+    <div className="flex flex-col min-h-screen bg-black relative items-between">
       <header className="px-4 lg:px-6 h-14 flex items-center">
         <Link className="flex items-center justify-center" href="#">
           <BookOpen className="h-6 w-6 mr-2" />
